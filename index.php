b165f9f43041d002646ef38b4e234b6b5d8908a3
Alim Din <ad432@njit.edu>
2017-10-24 20:18:02 -0400

    Query

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+
 echo "<h1>PDO!</h1>";
 $username = 'ad432';
 $password = 'qhDUhaQM';
@@ -10,9 +11,64 @@ $dsn = "mysql:host=$hostname;dbname=$username";
 try {
     $conn = new PDO($dsn, $username, $password);
     echo "Connected successfully<br>";
+
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
-$conn = null;
+
+// Select and Print
+try{
+	$sql = 'SELECT * FROM ad432.accounts
+          WHERE id < 6';
+    $result = $conn->prepare($sql);
+    $result->execute();
+
+    $count = $result->rowCount();
+    print("$count Records.<br>");
+
+    
+    
+    if($result->rowCount() > 0){
+        echo "<table>";
+            echo "<tr>";
+                echo "<th>id</th>";
+                echo "<th>email</th>";
+                echo "<th>fname</th>";
+                echo "<th>lname</th>";
+                echo "<th>phone</th>";
+                echo "<th>birthday</th>";
+                echo "<th>gender</th>";
+                echo "<th>password</th>";
+            echo "</tr>";
+        while($row = $result->fetch()){
+            echo "<tr>";
+                echo "<td>" . $row['id'] . "</td>";
+                echo "<td>" . $row['email'] . "</td>";
+                echo "<td>" . $row['fname'] . "</td>";
+                echo "<td>" . $row['lname'] . "</td>";
+                echo "<td>" . $row['phone'] . "</td>";
+                echo "<td>" . $row['birthday'] . "</td>";
+                echo "<td>" . $row['gender'] . "</td>";
+                echo "<td>" . $row['password'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        // Free result set
+        unset($result);
+    } else{
+        echo "Nothing matched your search result.";
+    }
+} 
+      catch(PDOException $e){
+      die("ERROR: Cant execute $sql. " . $e->getMessage());
+}
+
+
+
+
+// Close connection
+unset($conn);
+
 
 ?>
+
